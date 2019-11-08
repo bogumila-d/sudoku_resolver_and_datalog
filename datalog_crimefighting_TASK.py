820c744c7d569b942cfc6c8a76e1022f5e59dba0
Bogumila Dubel <bogumila.dubel@namics.com>
2019-11-08 08:17:10 +0100

    soloution lab ex 2 datalog

--- datalog_crimefighting_TASK.py
+++ datalog_crimefighting_TASK.py
@@ -14,89 +14,123 @@ from pyDatalog import pyDatalog
 # work through this code from top to bottom (in the way you would use a R or Jupyter notebook as well...) and write datalog clauses
 # and python code in order to solve the respective tasks. Overall, there are 7 tasks.
 # ---------------------------------------------------------------------------
-calls = pa.read_csv('calls.csv', sep='\t', encoding='utf-8')
-texts = pa.read_csv('texts.csv', sep='\t', encoding='utf-8')
 
-suspect = 'Quandt Katarina'
-company_Board = ['Soltau Kristine', 'Eder Eva', 'Michael Jill']
+@pyDatalog.program()
+def _():
 
-pyDatalog.create_terms('knows','has_link','many_more_needed')
-pyDatalog.clear()
+    calls = pa.read_csv('calls.csv', sep='\t', encoding='utf-8')
+    texts = pa.read_csv('texts.csv', sep='\t', encoding='utf-8')
+    suspect = 'Quandt Katarina'
+    company_Board = ['Soltau Kristine', 'Eder Eva', 'Michael Jill']
+    pyDatalog.create_terms('knows, '
+                           'has_link, '
+                           'all_connections, '
+                           'max_five_people, '
+                           'many_more_needed')
 
-# First, treat calls as simple social links (denoted as knows), that have no date
-for i in range(0,50):
-    +knows(calls.iloc[i,1], calls.iloc[i,2])
+    # First, treat calls as simple social links (denoted as knows), that have no date
+    # the caller knows callee
+    for i in range(len(calls)):
+        +knows(calls.iloc[i, 1], calls.iloc[i, 2])
+        # Task 1: Knowing someone is a bi-directional relationship -> define the predicate accordingly
+    knows(X, Y) <= knows(Y, X)
 
+    print(knows(suspect, Y))
 
+    # Task 2: Define the predicate has_link in a way that it is true if a connection exists
+    # (path of people knowing the next link)
+    # Hints:
+    # check if your predicate works: at least 1 of the following asserts should be true
+    # (2 if you read in all 150 communication records)
+    #   (be aware of the unusual behaviour that if an assert evaluates as true, an exception is thrown)
 
-# Task 1: Knowing someone is a bi-directional relationship -> define the predicate accordingly
+    has_link(X, Y) <= knows(X, Y)
+    has_link(X, Y) <= knows(X, Z) & has_link(Z, Y) & (X != Y)
 
+    assert (has_link('Quandt Katarina', company_Board[0]) == ())
+    # assert (has_link('Quandt Katarina', company_Board[1]) == ())
+    # assert (has_link('Quandt Katarina', company_Board[2]) == ())
 
+    # 'Quandt Katarina' knows 'Eder Eva' and 'Michael Jill'
 
+    # Task 3: You already know that a connection exists; now find the concrete paths between the board members
+    # and the suspect
+    # Hints:
+    #   if a knows b, there is a path between a and b
+    #   (X._not_in(P2)) is used to check whether x is not in path P2
+    #   (P==P2+[Z]) declares P as a new path containing P2 and Z
 
-# Task 2: Define the predicate has_link in a way that it is true if a connection exists (path of people knowing the next link)
-# Hints:
-#   check if your predicate works: at least 1 of the following asserts should be true (2 if you read in all 150 communication records)
-#   (be aware of the unusual behaviour that if an assert evaluates as true, an exception is thrown)
-#   assert (has_link('Quandt Katarina', company_Board[0]) == ())
-#   assert (has_link('Quandt Katarina', company_Board[1]) == ())
-#   assert (has_link('Quandt Katarina', company_Board[2]) == ())
+#     path(Y,Y,P) <=  (P==[Y])
+# path(X,Y,P) <= path(X,Z,P2) & knows(Z,Y) & (Y._not_in(P2)) &  (X!=Y)  & (P==P2 + [Y])
 
+    all_connections(X, Y, P) <= all_connections(X, Z, P2) & knows(Z, Y) & (X != Y) & (X._not_in(P2)) & (Y._not_in(P2)) & (P == P2+[Y])
+    all_connections(X, Y, P) <= knows(X, Y) & (P == [Y])
 
+    # Task 4: There are too many paths. We are only interested in short paths.
+    # Find all the paths between the suspect and the company board that contain five people or less
 
+    (max_five_people(X, Y, P, C)) <= (max_five_people(X, Z, P2, C2)) & knows(Z, Y) & (X != Y) \
+    & (X._not_in(P2)) & (Y._not_in(P2)) & (P == P2+[Z]) & (C == C2 + 1) & (C <= 2)
+    (max_five_people(X, Y, P, C)) <= knows(X, Y) & (P == []) & (C == 0)
 
-# Task 3: You already know that a connection exists; now find the concrete paths between the board members and the suspect
-# Hints:
-#   if a knows b, there is a path between a and b
-#   (X._not_in(P2)) is used to check whether x is not in path P2
-#   (P==P2+[Z]) declares P as a new path containing P2 and Z
+    for member in company_Board:
+        print("Who: ", suspect, "/", member)
+        print(max_five_people(suspect, member, P, C))
+        print("Who: ", member, "/", suspect)
+        print(max_five_people(member, suspect, P, C))
 
+    # ---------------------------------------------------------------------------
+    # Call-Data analysis:
+    # Now we use the text and the calls data together with their corresponding dates
+    # ---------------------------------------------------------------------------
+    date_board_decision = '12.2.2017'
+    date_shares_bought = '23.2.2017'
 
+    pyDatalog.create_terms('called, texted, descending_communication')
+    pyDatalog.clear()
+    for i in range(len(calls)):  # calls
+        +called(calls.iloc[i, 1], calls.iloc[i, 2], calls.iloc[i, 3])
+    for i in range(len(texts)):  # texts
+        +texted(texts.iloc[i, 1], texts.iloc[i, 2], texts.iloc[i, 3])
 
+    # calls are bi-directional
+    called(X, Y, Z) <= called(Y, X, Z)
 
-# Task 4: There are too many paths. We are only interested in short paths.
-# Find all the paths between the suspect and the company board that contain five people or less
+    # Task 5: Again we are interested in links, but this time a connection is only valid
+    # if the links are descending in date;
+    #         find out who could have actually sent the information by adding this new restriction
+    # Hints:
+    #   You are allowed to naively compare the dates lexicographically using ">" and "<";
+    #   it works in this example (but is evil in general)
 
+    print("descending_communication")
+    (descending_communication(X, Y, D, P)) <= \
+    (descending_communication(X, Z, D2, P2)) & (called(Z, Y, D) or texted(Z, Y, D)) & (X != Y) & \
+    (X._not_in(P2)) & (Y._not_in(P2)) & (P == P2+[D2]+[Y]+[D]) & \
+    (D >= date_board_decision) & (D <= date_shares_bought) & (D < D2) & (D2 >= date_board_decision) & (D2 <= date_shares_bought)
+    (descending_communication(X, Y, D, P)) <= called(X, Y, D) & (P == [Y])
 
+    # (descending_communication(X, Y, D, P)) <= \
+    # (descending_communication(X, Z, D2, P2)) & texted(Z, Y, D) & (X != Y) & \
+    # (X._not_in(P2)) & (Y._not_in(P2)) & (P == P2+[D2]+[Y]+[D]) & \
+    # (D >= date_board_decision) & (D <= date_shares_bought) & (D < D2) & (D2 >= date_board_decision) & (D2 <= date_shares_bought)
+    # (descending_communication(X, Y, D, P)) <= texted(X, Y, D) & (P == [Y])
 
+    # D2 ist das Datum des vorhergehenden Anrufs / Kommunikation
 
-# ---------------------------------------------------------------------------
-# Call-Data analysis:
-# Now we use the text and the calls data together with their corresponding dates
-# ---------------------------------------------------------------------------
-date_board_decision = '12.2.2017'
-date_shares_bought = '23.2.2017'
-pyDatalog.create_terms('called,texted')
-pyDatalog.clear()
+    # Task 6: Find all the communication paths that lead to the suspect
+    # (with the restriction that the dates have to be ordered correctly)
 
-for i in range(0,50): # calls
-    +called(calls.iloc[i,1], calls.iloc[i,2],calls.iloc[i,3])
+    for member in company_Board:
+        print("From: ", suspect, 'to: ', member)
+        print(descending_communication(suspect, member, D, P))
+        print("From: ", member, "to: ", suspect)
+        print(descending_communication(member, suspect, D, P))
 
-for i in range(0,50): # texts
-    +texted(texts.iloc[i,1], texts.iloc[i,2],texts.iloc[i,3])
+    # Final task: after seeing this information, who, if anybody, do you think gave a tip to the suspect?
 
-called(X,Y,Z) <= called(Y,X,Z) # calls are bi-directional
+    # EDER EVA
 
-
-
-# Task 5: Again we are interested in links, but this time a connection is only valid if the links are descending in date; 
-#         find out who could have actually sent the information by adding this new restriction
-# Hints:
-#   You are allowed to naively compare the dates lexicographically using ">" and "<";
-#   it works in this example (but is evil in general)
-
-
-
-
-# Task 6: Find all the communication paths that lead to the suspect (with the restriction that the dates have to be ordered correctly)
-
-
-
-
-# Final task: after seeing this information, who, if anybody, do you think gave a tip to the suspect?
-
-
-
-
-# General hint (only use on last resort!): 
-#   if nothing else helped, have a look at https://github.com/pcarbonn/pyDatalog/blob/master/pyDatalog/examples/graph.py
+    # General hint (only use on last resort!):
+    # if nothing else helped, have a look at
+    # https://github.com/pcarbonn/pyDatalog/blob/master/pyDatalog/examples/graph.py
